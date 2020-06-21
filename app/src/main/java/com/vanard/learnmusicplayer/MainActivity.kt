7a22bcc368bedc93b575bcd33b27c6049838826a
Vian RD <vianixa34@gmail.com>
2020-06-21 12:17:59 +0800

    Add tab indicator background, progress to make a request permission for storage, try to clean the code

--- app/src/main/java/com/vanard/learnmusicplayer/MainActivity.kt
+++ app/src/main/java/com/vanard/learnmusicplayer/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.vanard.learnmusicplayer
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
 import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
 import com.vanard.learnmusicplayer.ui.main.AlbumsFragment
@@ -12,12 +15,14 @@ import com.vanard.learnmusicplayer.ui.main.SongsFragment
 import com.vanard.learnmusicplayer.ui.main.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
-private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
-)
-
 class MainActivity : AppCompatActivity() {
+
+    private val TAB_TITLES = arrayOf(
+        R.string.tab_text_1,
+        R.string.tab_text_2
+    )
+
+    private val WRITE_REQUEST_CODE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +32,11 @@ class MainActivity : AppCompatActivity() {
 //        val tabs: TabLayout = findViewById(R.id.tabs)
 //        tabs.setupWithViewPager(viewPager)
 
+        permission()
+        setupViewPager()
+    }
+
+    private fun setupViewPager() {
         val fragmentList = arrayListOf<Fragment>(
             SongsFragment(),
             AlbumsFragment()
@@ -44,12 +54,44 @@ class MainActivity : AppCompatActivity() {
             TabConfigurationStrategy { tab, position ->
                 tab.text = this.resources.getString(TAB_TITLES[position])
             }).attach()
+    }
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
+    private fun permission() {
+        val permission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        } else {
+            Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            WRITE_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            WRITE_REQUEST_CODE -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show()
+                } else {
+                    makeRequest()
+                }
+
+            }
         }
     }
 }
